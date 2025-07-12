2fe3716a65927b1984ae3f97ccae398fff11e317
zues13bhai <hitesh.siwach032005@gmail.com>
2025-07-12 16:46:50 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -641,7 +641,8 @@ function App() {
 
         {/* About Section */}
         <section id="about" className="py-20 relative">
-          <TechBackground variant="minimal" />
+         <Orb />
+
           
           <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <motion.div
