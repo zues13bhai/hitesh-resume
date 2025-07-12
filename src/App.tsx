92897d9a7180ab58fd929a6c30ae8307f61eb8f1
zues13bhai <hitesh.siwach032005@gmail.com>
2025-07-12 16:47:15 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1234,7 +1234,8 @@ function App() {
 
         {/* Achievements Section */}
         <section id="achievements" className="py-20 relative">
-          <TechBackground variant="minimal" />
+         <Orb />
+
           
           <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <motion.div
