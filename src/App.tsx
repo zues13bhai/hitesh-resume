13e60b5b9fde89cca9a7ce74f0698f75fb81ecc5
zues13bhai <hitesh.siwach032005@gmail.com>
2025-07-12 16:47:27 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1356,7 +1356,8 @@ function App() {
 
         {/* Contact Section */}
         <section id="contact" className="py-20 relative">
-          <TechBackground variant="cyberpunk" />
+         <Orb />
+
           
           <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <motion.div
